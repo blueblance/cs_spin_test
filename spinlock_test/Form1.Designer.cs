2df7aad900e436196045d4d2da3f685645ff1236
blueblance <blueblance@gmail.com>
2019-01-30 14:28:25 +0800

    basic 1

--- spinlock_test/Form1.Designer.cs
+++ spinlock_test/Form1.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -39,22 +44,70 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(196, 37);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "5";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(325, 37);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(325, 83);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(902, 567);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "spin_test";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- spinlock_test/Form1.cs
+++ spinlock_test/Form1.cs
@@ -6,14 +6,71 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace spinlock_test
 {
     public partial class Form1 : Form
     {
+        UI_Control uic = new UI_Control();
+        ManualResetEvent mre = new ManualResetEvent(false);
+        static int testnum = 10;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t1 = new Thread(func_1);
+            Thread t2 = new Thread(func_2);
+            t1.Start(5);
+            t2.Start(7);
+
+        }
+
+        private void func_1(object a)
+        {
+            for(int i = 0; i < 200; i++)
+            {
+                int temp = ((int)a * i);
+                uic.writetextbox(textBox1, temp.ToString());
+                testnum = temp;
+                uic.writetextbox(textBox4, (testnum - temp).ToString());
+                //Thread.Sleep(500);
+            }
+            
+        }
+
+        private void func_2(object a)
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                int temp = ((int)a * i);
+                uic.writetextbox(textBox2, temp.ToString());
+                testnum = temp;
+                uic.writetextbox(textBox5, (testnum - temp).ToString());
+                //Thread.Sleep(500);
+                
+            }
+        }
+    }
+
+    class UI_Control
+    {
+        public delegate void textboxhandler(TextBox tb, string text);
+        public void writetextbox(TextBox tb, string text)
+        {
+            if (tb.InvokeRequired)
+            {
+                textboxhandler th = new textboxhandler(writetextbox);
+                tb.Invoke(th, tb, text);
+            }
+            else
+            {
+                tb.Text = text;
+            }
         }
     }
 }
